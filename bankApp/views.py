84ef33ac638b88f893521a8cd1394a395f5d222a
Sofia Hernandez <sofia.hdezm1@gmail.com>
2022-04-25 12:37:14 -0500

    isolated en las transacciones

--- bankApp/views.py
+++ bankApp/views.py
@@ -13,39 +13,45 @@ from django.db import transaction
 
 @transaction.atomic
 # para poder hacer ATOMIC las transacciones
-# Hace como un borrador de todas las querys en .save() y 
+# SE EJECUTAN TODAS O NINGUNA
+# Hace como un borrador de todas las querys en .save() y
 # si una no funciona de manera correcta NO se hace ninguna
 def procesador_pagos(request):
     # form = Pago()
     # context = {
     #     'form': form,
     # }
-    
+
     if request.method == 'POST':
         # Creación del formulario
         form = Pago(request.POST)
-        
+
         # Validar el formulario
         if form.is_valid():
             envia_nombre = form.cleaned_data['envia']  # nombre de quien envía
-            recibe_nombre = form.cleaned_data['recibe']  # nombre de quien recibe
-            monto_data = decimal.Decimal(form.cleaned_data['monto'])  # monto en decimales
-            
-            envia_persona = cliente.objects.get(nombre=envia_nombre)
+            # nombre de quien recibe
+            recibe_nombre = form.cleaned_data['recibe']
+            monto_data = decimal.Decimal(
+                form.cleaned_data['monto'])  # monto en decimales
+
+            # envia_persona = cliente.objects.get(nombre=envia_nombre)
+            envia_persona = cliente.objects.select_for_update().get(
+                                    nombre=envia_nombre)  # para Isolated
             envia_persona.saldo -= monto_data  # se le RESTA el monto a lo que tiene
             envia_persona.save()
-            
-            recibe_persona = cliente.objects.get(nombre=recibe_nombre)
+
+            # recibe_persona = cliente.objects.get(nombre=recibe_nombre)
+            recibe_persona = cliente.objects.select_for_update().get(
+                                    nombre=recibe_nombre)  # para Isolated
             recibe_persona.saldo += monto_data  # se le SUMA el monto a lo que tiene
             recibe_persona.save()
-            
+
             return HttpResponseRedirect('bankApp')
     else:
         form = Pago()
-    
-    
+
     context = {
         'form': form,
     }
-    
+
     return render(request, 'index.html', context)
