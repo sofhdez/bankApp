cbc39b80dc59b0b47b93df3448cf32d3ed7c7c08
Sofia Hernandez <sofia.hdezm1@gmail.com>
2022-04-25 11:39:56 -0500

    admin model y forms.py

--- bankApp/admin.py
+++ bankApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import cliente
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'saldo')
+
+
+admin.site.register(cliente, ClienteAdmin)
