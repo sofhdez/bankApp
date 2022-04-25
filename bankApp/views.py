d22ff6103cb4eda35edcde45e4d8fb6005df860f
Sofia Hernandez <sofia.hdezm1@gmail.com>
2022-04-25 12:05:21 -0500

    creación de views, urls y modif de settings.py

--- bankApp/views.py
+++ bankApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .forms import Pago  # form que creamos
+
+
+def procesador_pagos(request):
+    form = Pago()
+    context = {
+        'form': form,
+    }
+    return render(request, 'index.html', context)
